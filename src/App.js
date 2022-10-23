ca27aa8670ed283611b245b714c6e0f88b52f5bf
Rex <leerenxiang.12@gmail.com>
2022-10-23 10:02:08 +0800

    commit sg freevsl changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
-// import React, {useState} from "react";
-// import ViewPerformancePage from "./pages/sgfreevsl/sgfreevsl";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <ViewPerformancePage/>
-//     </div>
-//   );
-// }
-// export default App;
-
+import {apiEndpoint} from "./common";
 import React, { useEffect, useState } from 'react';
+import {getRequest} from "./requestBuilder";
+
+import ViewPerformancePage from "./pages/sgfreevsl/sgfreevsl"
 
 function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
     (async () => {
-    const res = await fetch ("https://cape-webapp.herokuapp.com/api/SGfreeVsl");
+    const res = await fetch (apiEndpoint + "/SGfreeVsl");
     const json = await res.json();
     setData(json);
   })();
   }, []);
 
 return <p>data: {JSON.stringify(data)}</p>;
-}
 
+// const [isLoading, setIsLoading] = useState(false)
+// useEffect(() => {
+//   refreshSalesFigure();
+// }, [])
+
+// const refreshSalesFigure = async () => {
+//   setIsLoading(true)
+//   const salesFigureObj = await getRequest(apiEndpoint + '/SGfreeVsl')
+//   // const salesFigure = salesFigureObj.data.salesFigure
+
+//   return <p>data: {JSON.stringify()}</p>;
+// }
+}
 export default App
