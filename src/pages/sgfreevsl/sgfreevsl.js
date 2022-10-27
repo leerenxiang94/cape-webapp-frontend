a14c09f3fe922f8c6a0f8941e6f21746f6ceb748
Rex <leerenxiang.12@gmail.com>
2022-10-27 20:25:34 +0800

    added search function to sgfreevsl

--- src/pages/sgfreevsl/sgfreevsl.js
+++ src/pages/sgfreevsl/sgfreevsl.js
@@ -8,8 +8,11 @@ import {getRequest} from "../../requestBuilder";
 import {apiEndpoint} from "../../common";
 import '../../Home.css';
 
+
 const ViewSGfreevslpage = (props) => {
+    
 const [response, setResponse] = useState([]) 
+const [searchTerm, setSearchTerm] = useState("")
 
 useEffect(() => {
     refreshSGFreevsl();
@@ -17,30 +20,9 @@ useEffect(() => {
 
 const refreshSGFreevsl = async () => {
     const pendingSGfreevslObject = await getRequest(apiEndpoint + '/SGfreeVsl')
-    const SGFreevslFigure2 = pendingSGfreevslObject.data.SGfreeVsl.values
-    const SGFreevslFigure = 
-    <table className = "freevsl-figure-row-figure">
-  <tr>
-          <th>Vsl Name</th>
-          <th>DWT</th>
-          <th>BUILT YEAR</th>
-          <th>ETA S.BAY</th>
-          <th>ETA Tub</th>
-        </tr>
-    {
-    SGFreevslFigure2.map( name => 
-    <tr>
-        <td>{name[0]}{" "}</td>
-        <td>{name[1]}</td>
-        <td>{name[2]}{" "}</td>
-        <td>{name[3].replace("S.Bay","")}{" "}</td>
-        <td>{name[4].replace("Tub","")}</td>
-        </tr>)}
-  </table>
+    const SGFreevslFigure = pendingSGfreevslObject.data.SGfreeVsl.values
     setResponse(SGFreevslFigure)
 }
-
-
 
 return (
     <div className="App">
@@ -55,10 +37,48 @@ return (
         <div className="logo-container interactive" onClick={() => props.navigate("")}>
             <img src={logo} className="submit-order-app-logo" alt="logo"/>
         </div>
-     
   <div>
+  <h1 className = "freevsl-figure-row-figure" >SG Free Vsl - Heading West</h1>
+    <input className = "searchbarwidth"
+    type = "text"
+    placeholder = "Type text to search Vsl/DWT/ETA..."
+    onChange = {(event) => {
+        setSearchTerm(event.target.value);
+    }}></input>
     
-    {response}
+     <table className = "freevsl-figure-row-figure">
+  <tr>
+          <th>Vsl</th>
+          <th>DWT</th>
+          <th>YR</th>
+          <th>ETA S.Bay</th>
+          <th>ETA Tub</th>
+        </tr>
+    {
+    response.filter((val)=> {
+        if (searchTerm === "") {
+            return val
+        } else if (val[0].toLowerCase().includes(searchTerm.toLowerCase()))  
+        { return val
+        } else if (val[1].toLowerCase().includes(searchTerm.toLowerCase()))
+        { 
+            return val
+        } else if (val[3].toLowerCase().includes(searchTerm.toLowerCase()))
+        { 
+            return val
+        } else if (val[4].toLowerCase().includes(searchTerm.toLowerCase()))
+        { 
+            return val
+        }
+    }).map( name => 
+    <tr>
+        <td>{name[0]}</td>
+        <td>{name[1]}</td>
+        <td>{name[2]}</td>
+        <td>{name[3].replace("S.Bay","")}</td>
+        <td>{name[4].replace("Tub","")}</td>
+        </tr>)}
+  </table>
   </div>
     </div>
 )
